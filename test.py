ed8d30a9fd9ec906334ee96b16f87432f4d97450
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2019-11-17 01:04:43 +0530

    Train CNN

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from mpl_toolkits.mplot3d import Axes3D
 import scipy.io
 import math
-
+from keras.layers import Convolution1D, Dense, MaxPooling1D, Flatten
+from keras.models import Sequential
 
 def load_dataset():
 	mat_X = scipy.io.loadmat('X_data.mat')['ecg_in_window']
@@ -25,6 +22,37 @@ def load_dataset():
 
 	return X,y,X_test,y_test
 
-X,y,X_test,y_test = load_dataset()
+def evaluate_model(X, y, X_test, y_test):
+	k = 41
+	epochs = 10
+	model = Sequential()
+	# model.add(Reshape((1000,1),input_shape=(1000,)))
+	model.add(Convolution1D(filters = 1, kernel_size = k, activation = 'relu', input_shape = (1000,1)))
+	# model.add(GlobalAveragePooling1D())
+	model.add(Dense(100, activation = 'softmax'))
+	model.add(Dense(2, activation = 'softmax'))
+	model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['binary_accuracy'])
+
+	# fit network
+	model.fit(X, y, epochs = epochs, batch_size = 800, verbose = 0)
+
+	# evaluate network
+	_, accuracy = model.evaluate(X_test, y_test, batch_size = 200, verbose = 0) 
+
+	return accuracy
+
+def run_experiment(repeats):
+	X, y, X_test, y_test = load_dataset()
+	scores = list()
+
+	for r in range(repeats):
+		score = evaluate_model(X, y, X_test, y_test)
+		score = score*100.0
+		scores.append(score)
+
+	print(scores)
+
+run_experiment(10)
+
 
 
