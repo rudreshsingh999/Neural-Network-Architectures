be24f88e97eb05d99dbc2caa69b9f0c25572ccf5
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2019-11-17 19:12:01 +0530

    Final CNN model

--- test.py
+++ test.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 import scipy.io
 import math
-from keras.layers import Convolution1D, Dense, MaxPooling1D, Flatten
+import keras
+# from sklearn.cross_validation import train_test_split
+# from keras.layers import Convolution1D, Dense, MaxPooling1D, Flatten, Activation, GlobalAveragePooling1D
+from keras.layers import *
 from keras.models import Sequential
 
 def load_dataset():
 	mat_X = scipy.io.loadmat('X_data.mat')['ecg_in_window']
 	data_X = np.array(mat_X)
-	print(data_X.shape)
 	mat_y = scipy.io.loadmat('y_data.mat')['label']
 	data_y = np.array(mat_y)
 	data = np.append(data_X,data_y,axis = 1)
@@ -18,40 +20,58 @@ def load_dataset():
 	y = data[0:800 , -1]
 	X_test = data[801:1000 , :-1]
 	y_test = data[801:1000 , -1]
-	print(X.shape[0])
 
+	# X, y, X_test, y_test = train_test_split(data_X, data_y, test_size = 0.2, random_state = 42)
+
+	X = np.expand_dims(X,axis=2)
+	y = np.expand_dims(y,axis=2)
+	X_test = np.expand_dims(X_test,axis=2)
+	y_test = np.expand_dims(y_test,axis=2)
+
+	print(X.shape[0])
+	print(X.shape)
 	return X,y,X_test,y_test
 
 def evaluate_model(X, y, X_test, y_test):
-	k = 41
-	epochs = 10
+	k = 3
+	epochs = 20
 	model = Sequential()
-	# model.add(Reshape((1000,1),input_shape=(1000,)))
-	model.add(Convolution1D(filters = 1, kernel_size = k, activation = 'relu', input_shape = (1000,1)))
-	# model.add(GlobalAveragePooling1D())
-	model.add(Dense(100, activation = 'softmax'))
-	model.add(Dense(2, activation = 'softmax'))
-	model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['binary_accuracy'])
+	model.add(Convolution1D(filters = 32, kernel_size = k, activation = 'relu', input_shape = (1000,1)))
+	# model.add(Convolution1D(filters = 32, kernel_size = k, activation = 'relu'))
+	model.add(MaxPooling1D(2))
+	model.add(Convolution1D(filters = 64, kernel_size = k, activation = 'relu'))
+	model.add(MaxPooling1D(2))
+	# model.add(Convolution1D(filters = 64, kernel_size = k, activation = 'relu'))
+	model.add(GlobalAveragePooling1D())
+	# model.add(Dropout(0.5))
+	model.add(Dense(100, activation = 'relu'))
+	model.add(Dense(1, activation = 'sigmoid'))
+	print(model.summary())
 
+	model.compile(loss = 'mse', optimizer = 'rmsprop', metrics = ['accuracy'])
 	# fit network
-	model.fit(X, y, epochs = epochs, batch_size = 800, verbose = 0)
-
+	history = model.fit(X, y, batch_size = 2, epochs = epochs)
+	print(history)
 	# evaluate network
-	_, accuracy = model.evaluate(X_test, y_test, batch_size = 200, verbose = 0) 
+	loss , accuracy = model.evaluate(X_test, y_test, batch_size = 2) 
 
-	return accuracy
+	return loss,accuracy
 
 def run_experiment(repeats):
 	X, y, X_test, y_test = load_dataset()
 	scores = list()
-
+	losss = list()
 	for r in range(repeats):
-		score = evaluate_model(X, y, X_test, y_test)
+		loss,score = evaluate_model(X, y, X_test, y_test)
 		score = score*100.0
+		loss = loss*100.0
 		scores.append(score)
+		losss.append(loss)
 
-	print(scores)
+	print(scores,losss)
 
+# X, y, X_test, y_test = load_dataset()
+# print(evaluate_model(X, y, X_test, y_test))
 run_experiment(10)
 
 
